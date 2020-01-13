2f9b083624e62c968fa0eac287b45a4a2f10ff7d
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-01-13 12:51:37 +0200

    Project setup

---
+++ GitHubTimeMachine/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace GitHubTimeMachine
+{
+    public class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
