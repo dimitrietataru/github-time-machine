b1d551084876788a9b0fd408904a31884f1cc4bd
Dimitrie Tataru <dimitrie.tataru@assist.ro>
2020-01-14 11:13:43 +0200

    Implement file service: create, append, delete

--- GitHubTimeMachine/Interfaces/IFileService.cs
+++ GitHubTimeMachine/Interfaces/IFileService.cs
@@ -1,9 +1,11 @@
-﻿namespace GitHubTimeMachine.Interfaces
+﻿using System.Threading.Tasks;
+
+namespace GitHubTimeMachine.Interfaces
 {
     internal interface IFileService
     {
         void Create(string filePath);
-        void Update(string filePath);
+        Task AppendAsync(string filePath, string content);
         void Delete(string filePath);
     }
 }
--- GitHubTimeMachine/Services/FileService.cs
+++ GitHubTimeMachine/Services/FileService.cs
@@ -1,6 +1,43 @@
-﻿namespace GitHubTimeMachine.Services
+﻿using GitHubTimeMachine.Interfaces;
+using System.IO;
+using System.Threading.Tasks;
+
+namespace GitHubTimeMachine.Services
 {
-    internal sealed class FileService
+    internal sealed class FileService : IFileService
     {
+        public void Create(string filePath)
+        {
+            if (File.Exists(filePath))
+            {
+                return;
+            }
+
+            using var stream = File.CreateText(filePath);
+            stream.Close();
+        }
+
+        public async Task AppendAsync(string filePath, string content)
+        {
+            if (!File.Exists(filePath))
+            {
+                return;
+            }
+
+            using var stream = File.AppendText(filePath);
+            await stream.WriteLineAsync(content);
+
+            stream.Close();
+        }
+
+        public void Delete(string filePath)
+        {
+            if (!File.Exists(filePath))
+            {
+                return;
+            }
+
+            File.Delete(filePath);
+        }
     }
 }
